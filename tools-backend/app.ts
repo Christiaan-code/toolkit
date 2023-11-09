c0b0fe2d11488cec9377e86845ee5174b5ce4cb1
Christiaan du Preez <christiaan.d@jobjack.co.za>
2023-11-09 09:53:54 +0200

    The start of something new

---
+++ tools-backend/app.ts
@@ -0,0 +1,47 @@
+import { CryptoHandler } from "./CryptoHandler";
+import './env.config'
+
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser');
+const cors = require('cors')
+const port = 1000
+
+app.use(cors())
+
+app.get('/', (req, res) => {})
+
+app.use(bodyParser.json());
+
+app.post('/decrypt', (req, res) => {
+  const requestData: string = req.body.data
+  console.log(requestData)
+
+  const decryptedValue = CryptoHandler.decrypt(requestData)
+
+  res.json({ message: 'Received POST data', data: decryptedValue.toString() });
+});
+
+app.post('/encrypt', (req, res) => {
+  const requestData: string = req.body.data
+  console.log(requestData)
+
+  const decryptedValue = CryptoHandler.encrypt(requestData)
+
+  
+  res.json({ message: 'Received POST data', data: decryptedValue.toString() });
+});
+
+app.post('/one-way-encrypt', (req, res) => {
+  const requestData: string = req.body.data
+  console.log(requestData)
+
+  CryptoHandler.OneWayEncrypt(requestData).then((val) => {
+    res.json({ message: 'Received POST data', data: val.toString() });
+  })
+});
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
